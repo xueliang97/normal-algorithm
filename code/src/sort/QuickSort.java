30373308872da6d32312b931aa4bf0c1361c4d50
xueliang <xueliangddsb@gmail.com>
2020-04-19 15:56:54 +0800

    快排

---
+++ code/src/sort/QuickSort.java
@@ -0,0 +1,42 @@
+package sort;
+
+import java.util.Arrays;
+
+public class QuickSort {
+
+    public static void quickSort(int[] nums,int low,int height){
+        if(low>=height)
+            return ;
+        int i = getIndex(nums,low,height);//划分枢纽 取开头为枢纽从后往前，当height值<枢纽值
+        // height值赋给low,从前往后;当low值>枢纽值 low赋给height值
+        quickSort(nums,low,i-1);
+        quickSort(nums,i+1,height);
+    }
+
+    public static void main(String[] args){
+        int[] nums = {9,5,6,2,3,7,3,0};
+        quickSort(nums,0,nums.length-1);
+        for(int k:nums)
+            System.out.print(k+" ");
+    }
+    public static int getIndex(int[] nums,int low,int height){//获得枢纽索引值
+        int temp = nums[low]; //保存枢纽值
+        while(low<height){
+            while(low<height&&nums[height]>=temp){
+                height--;
+            }
+            nums[low]  = nums[height];
+
+            while(low<height&&nums[low]<=temp){
+                low++;
+            }
+            nums[height] = nums[low];
+
+        }
+
+        nums[low] = temp;//返回时height = low;
+        return low;
+
+
+    }
+}
